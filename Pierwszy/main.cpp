7032c75eb7eb6d85ce25173af78e9869f4f959b7
Maciej <304175@uwr.edu.pl>
2019-10-16 14:53:47 +0200

    test

--- Pierwszy/main.cpp
+++ Pierwszy/main.cpp
@@ -5,13 +5,9 @@ using namespace std;
 
 int main()
 {
-    int a;
-
-<<<<<<< HEAD
+    int b;
     cout << "Nie Edycja Wojciecha. Narazko" << endl;
     string a;
-=======
     std::string a;
->>>>>>> cec8322fc322d0f207b5d38dd43c43ae2dc7bf2e
     return 0;
 }

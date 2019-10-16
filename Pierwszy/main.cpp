cf7aae5fee875beff382e62a160fb85a8740b92b
Wojtek <subidubidan@gmail.com>
2019-10-16 15:10:00 +0200

    asdasdasd

--- Pierwszy/main.cpp
+++ Pierwszy/main.cpp
@@ -7,6 +7,6 @@ int main()
 {
     std::cin >> x,a,b,c;
     auto lambda =[]( float x, float a, float b, float c){ return a*pow(x,2)+b*x + c;};
-    cout << lambda(x,a,b,c) << "XD";
+    cout << lambda(x,a,b,c) << "XD" << "asdasdsa";
     return 0;
 }

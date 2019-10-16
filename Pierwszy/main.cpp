845d7183850593c29ae085ea3dfea337d63a18ea
Maciej <304175@uwr.edu.pl>
2019-10-16 15:15:46 +0200

    pierwszy commit na gałęzi

--- Pierwszy/main.cpp
+++ Pierwszy/main.cpp
@@ -1,13 +1,14 @@
 #include <iostream>
 #include<cmath>
-
+#include<vector>
+#include<cstdio>
 using namespace std;
 
+vector<int> test(100, 857142);
+int sum=0;
 int main()
 {
-    int b;
-    cout << "Nie Edycja Wojciecha. Narazko" << endl;
-    string a;
-    std::string a;
-    return 0;
+    for (auto x:test)
+        sum+=x;
+    printf("%d\n", suma);
 }

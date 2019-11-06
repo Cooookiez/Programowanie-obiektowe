eabfdcac7830fccb97039b737a6676d971af40da
Wojtek <subidubidan@gmail.com>
2019-11-06 15:01:15 +0100

    adsa

--- Pierwszy/tests/test1.cpp
+++ Pierwszy/tests/test1.cpp
@@ -1,4 +1,17 @@
 #include <gtest/gtest.h>
+#include "../kwadratowa.h"
+
+TEST(prosty_test, test_wartosci)
+{
+   const float tolerance = 1e-5;
+   float a = 1;
+   float b = 4;
+   float c = -5;
+   auto val = kwadratowa(a, b, c );
+
+   EXPECT_NEAR(val.first, -5.0, tolerance);
+   EXPECT_NEAR(val.second, 1.0, tolerance);
+}
 
 int main(int argc, char **argv) {
     ::testing::InitGoogleTest(&argc, argv);

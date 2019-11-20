792f6e720dbda6fac987b88f4871ebb72d8e95e5
Wojtek <subidubidan@gmail.com>
2019-11-20 14:40:02 +0100

    jklasdha

--- build-Pierwszy-Desktop-Debug/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/wstrak/Pulpit/po/Programowanie-obiektowe/Pierwszy
-# Build directory: /home/wstrak/Pulpit/po/Programowanie-obiektowe/build-Pierwszy-Desktop-Debug
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
